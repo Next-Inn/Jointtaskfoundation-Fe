ef95bfb148817128ffcef81c2c2bd3d7125ed2bc
Tueloper <ozurumbatochukwu@yahoo.com>
2020-03-14 00:55:25 +0100

    Header fixed using vue class rendering

---
+++ components/partials/HomeNavbar.vue
@@ -0,0 +1,126 @@
+<template>
+  <div>
+    <nav
+      id="navBar"
+      class="navbar navbar-expand-md navbar-dark fixed-top"
+      :class="[scroller ? backgroundColored : backgroundTransparent ]"
+    >
+      <div class="container">
+        <nuxt-link to="/" class="navbar-brand">JTF</nuxt-link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <nuxt-link to="/" class="nav-link" id="home">Home</nuxt-link>
+            </li>
+            <li class="nav-item">
+              <nuxt-link to="/about" class="nav-link" id="about-nav">About</nuxt-link>
+            </li>
+            <li class="nav-item">
+              <nuxt-link to="plan" class="nav-link" id="plan-nav">Marketing Plan</nuxt-link>
+            </li>
+            <li class="nav-item">
+              <nuxt-link to="contact" class="nav-link" id="contact-nav">Contact</nuxt-link>
+            </li>
+            <li class="nav-item">
+              <nuxt-link to="/user/login" class="nav-link" id="login-nav">Log in</nuxt-link>
+            </li>
+            <li class="nav-item">
+              <nuxt-link to="/user/signup" class="nav-link" id="signUp-nav">Sign Up</nuxt-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- end of nav -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeNavbar',
+  data() {
+    return {
+      isScrolling: false,
+      backgroundTransparent: ['nav-transparent'],
+      backgroundColored: ['nav-colored']
+    }
+  },
+
+  mounted() {
+    document.addEventListener('scroll', this.scrollFunction)
+  },
+
+  computed: {
+    scroller() {
+      return this.isScrolling
+    }
+  },
+
+  methods: {
+    scrollFunction() {
+      if (
+        document.body.scrollTop < 0 ||
+        document.documentElement.scrollTop > 5
+      ) {
+        return (this.isScrolling = true)
+      } else {
+        return (this.isScrolling = false)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* navigation */
+
+.navbar {
+  text-transform: uppercase;
+  font-weight: 700;
+  font-size: 0.9rem;
+  letter-spacing: 0.1rem;
+}
+
+.navbar-brand {
+  font-size: 2rem;
+}
+
+.navbar-nav li {
+  padding-right: 0.7rem;
+}
+
+.navbar-dark .navbar-nav .nav-link {
+  color: #fff;
+  padding-top: 0.8rem;
+  font-size: 16px;
+}
+
+.navbar-dark .navbar-nav .nav-link.active,
+.navbar-dark .navbar-nav .nav-link:hover {
+  color: #5ef4da;
+}
+
+.nav-transparent {
+  /* background: rgba(0, 0, 0, 0.18) !important; */
+  transition: 2s;
+}
+
+.nav-colored {
+  background: var(--main-bg-color) !important;
+  color: #fff !important;
+  text-transform: uppercase;
+  font-weight: 700;
+  font-size: 0.9rem;
+  letter-spacing: 0.1rem;
+  transition: 2s;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,50 +1,34 @@
 <template>
   <div>
-    <Navbar v-on:scroll="changeNav"/>
-    
+    <Navbar />
+
     <nuxt />
-    <Footer/>
+    <Footer />
   </div>
 </template>
 
 <script>
-import Navbar from '~/components/partials/Navbar'
+import Navbar from '~/components/partials/HomeNavbar'
 import Footer from '~/components/partials/Footer'
 export default {
   components: {
     Navbar,
     Footer
-  }, 
-  methods: {
-    changeNav() {
-      window.onscroll = function(){
-    const navItem = document.getElementById("navBar");
-    if(document.body.scrollTop < 0 || document.documentElement.scrollTop > 5){
-      navItem.classList.add("nav-colored");
-      navItem.classList.remove("nav-transparent");
-    }else{
-      navItem.classList.add("nav-transparent");
-      navItem.classList.remove("nav-colored");
-    }
-  }
-    }
-    
   }
 }
 </script>
 
 <style>
-
 :root {
-	--main-bg-color: #23395d;
-	--main-txt-color: blue;
-	--white: #fff;
-	--main-padding: 15px;
+  --main-bg-color: #23395d;
+  --main-txt-color: blue;
+  --white: #fff;
+  --main-padding: 15px;
 }
 body {
-	overflow-x: hidden;
-	font-family: 'Nunito', sans-serif;
-	color: #505962;
+  overflow-x: hidden;
+  font-family: 'Nunito', sans-serif;
+  color: #505962;
 }
 
 html {
@@ -67,35 +51,14 @@ html {
 }
 
 .btn-blue {
-	background: #fff;
-	color: #000;
-	border: 1px solid #000;
+  background: #fff;
+  color: #000;
+  border: 1px solid #000;
 }
 
 .btn-blue:hover {
-	background: var(--main-bg-color);
-	color: #fff;
-	border: 1px solid var(--main-bg-color);
+  background: var(--main-bg-color);
+  color: #fff;
+  border: 1px solid var(--main-bg-color);
 }
-
-  .nav-transparent {
-	background: rgba(0, 0, 0, 0.18) !important;
-	transition: 2s;
-    }
-
-    .nav-colored {
-        background: var(--main-bg-color) !important;
-        color: #fff !important;
-        text-transform: uppercase;
-        font-weight: 700;
-        font-size: .9rem;
-        letter-spacing: 0.1rem;
-        transition: 2s;
-    }
-
-
-
-
-
-
 </style>
