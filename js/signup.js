a229248b15e0300277f547c958376e3d1eabeb0e
Odomfavour <ogochukwuodom@gmail.com>
2020-03-14 00:57:49 +0100

    FE/ Created admin signup and login forms

--- js/signup.js
+++ js/signup.js
@@ -20,4 +20,14 @@ async function signUpUser(e) {
     const userName = document.querySelector('#username').value;
     const password = document.querySelector('#password').value;
     const userName = document.querySelector('#username').value;
+
+
+    if(!Fname || !Lname || !email || !phoneNumber || !password || !confirmPassword || !password || !userName)  {
+        ui.printMessage('Fill all Fields!!!', 'alert-danger');
+    }else if (password != confirmPassword) {
+        ui.printMessage('Password Must be Equal to Confirm Password', 'alert-danger');
+    }else {
+        ui.printMessage('Thank You for Your Info, Processing!', 'alert-success');
+        
+        // Get user details
 }
