d576166751f182722da5aebf4650d309cee8c19f
Tueloper <ozurumbatochukwu@yahoo.com>
2020-04-11 15:03:15 +0100

    :guitar: user can get a new email token

--- pages/verify/verifyUser.vue
+++ pages/verify/verifyUser.vue
@@ -7,11 +7,38 @@
       <p class="card-text mb-5">
         Please Your Verification Mail Has Been Sent, Please Check Your Email
         <br />Click On The Link and You Will Be Redirected To Login To Your Account.
+        <br />If You didn't get the mail, please click
+        <span @click:prevent="getNewToken">Resend</span>
       </p>
       <p class="mb-5">Thank You Once More 🤗....</p>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: ''
+    }
+  },
+
+  mounted() {
+    return (this.email = this.$store.getters['user/getSingleEmail'])
+  },
+
+  methods: {
+    async getNewToken() {
+      try {
+        const body = {
+          email: this.email
+        }
+        await this.$axios.post('auth/refresh-email-token', body)
+      } catch (e) {}
+    }
+  }
+}
+</script>
 
 <script>
 export default {
